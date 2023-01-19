09e072205f4c020abd024227c9bed5601dae1baa
Wael El Echi <echi.wael@gmail.com>
2023-01-19 03:04:09 +0100

    update server uri

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { LAYOUT, MENU_BEHAVIOUR, NAV_COLOR, MENU_PLACEMENT, RADIUS, THEME_COLOR,
 
 export const IS_DEMO = false;
 export const IS_AUTH_GUARD_ACTIVE = true;
-export const SERVICE_URL = 'https://where-iz-api.onrender.com/api/v1';
+export const SERVICE_URL = 'https://where-iz-api-v3.onrender.com/api/v1';
 // export const SERVICE_URL = 'http://localhost:5000/api/v1';
 
 export const USE_MULTI_LANGUAGE = false;
